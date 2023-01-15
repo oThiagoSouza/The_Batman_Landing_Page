6ac6f540778eefe042bb5432f923096009621b32
Thiago Souza <souzaalvesthiago99@gmail.com>
2023-01-15 20:09:05 -0300

    finzalizando projeto

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require ('gulp-uglify');
 
+function scripts () {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'));
+}
+
 function imageCompress () {
     return gulp.src('./src/images/**/*')
     .pipe(imagemin())
@@ -19,4 +25,5 @@ exports.default = gulp.parallel(imageCompress);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,43 @@
 var indiceAtual = 1;
 displaySlides(indiceAtual);
+const inpt = document.getElementById('email')
+const btn = document.querySelector('.hero__form__btn')
+const msg = document.getElementById('hero__msgEnvio')
+const heroSection = document.querySelector('.hero')
+const heroHeight = heroSection.clientHeight
+
+window.addEventListener('scroll', function(){
+    const posicaoAtual = window.scrollY
+    if(posicaoAtual < heroHeight) {
+        ocultaElementosHeader()
+    } else {
+        exibeElementosHeader()
+    }
+})
+
+function ocultaElementosHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosHeader(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
+// Exibe mensagem de envio form
+btn.addEventListener('click', mostraMsg)
+
+function mostraMsg() {
+    if(inpt.value == '') {
+        alert('Insira seu e-mail para poder enviar.')
+    } else {
+        inpt.style.display = 'none'
+        msg.innerHTML = 'Seu envio foi concluído!'
+    }
+}
+
+// Slider Personanges
 
 function setSlides(num) {
     displaySlides(indiceAtual += num);
